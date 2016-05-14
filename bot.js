042120da0a5bce5307e6561de069a0bfd80e71dc
tedunni <tedunni@gmail.com>
2016-05-14 18:57:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -172,6 +172,42 @@ function postMessage(response) {
   botReq.end(JSON.stringify(body));
 }
 
+function getMessage(response) {
+  var botResponse,options, body, botReq;
+
+  botResponse = response
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
