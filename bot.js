93d1f5ed378473d169c6e16c0adef7e82ad74116
tedunni <tedunni@gmail.com>
2016-05-14 19:11:51 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,6 +13,7 @@ function respond() {
       botRegexshovel = /^\/shovel/;
       botRegexredditlink = /^\/redditlink/;
       botRegexapplicationlink = /^\/applicationlink/;
+      botRegexgetsomething = /^\/getsomething/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
