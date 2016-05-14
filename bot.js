6f97e98623153553f2ba1ddb9c3e9bdcd18a51fa
tedunni <tedunni@gmail.com>
2016-05-14 18:31:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/;  botRegexTed=/^\/everyone/;botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
+      botRegex = /^\/cool guy/;  botRegexTed=/^\/everyone/;botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexWarmatchguide = /^\/warmatchguide/
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; 
       botRegexCC = /^\/cc/;
@@ -38,9 +38,9 @@ function respond() {
     postMessage("http://www.hackcollege.com/wp-content/uploads/2013/02/kno_advance.jpg");
     this.res.end();
   }
-  else if(request.text && botRegexRules.test(request.text)) {
+  else if(request.text && botRegexWarmatchguide.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://docs.google.com/document/d/1hSuEG7oplnx4IX6HGsMOjsWb9TCqC4-F1NLjuBz5PCM/edit");
+    postMessage("https://docs.google.com/document/d/1ZxGtOBEeh7TJMNFCW72pzVxWLJTkEiGXFs6YeZ0Yv5g");
     this.res.end();
   } 
   else if(request.text && botRegexGTA.test(request.text)) {
