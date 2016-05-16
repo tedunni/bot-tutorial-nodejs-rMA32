8af1fa7e7e211569dd3d7420946f2c8669243778
tedunni <tedunni@gmail.com>
2016-05-16 15:34:14 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,7 @@ function respond() {
       botRegexredditlink = /^\/redditlink/;
       botRegexapplicationlink = /^\/applicationlink/;
       botRegexgetsomething = /^\/getsomething/;
+      botRegexhaters = /^\/haters/;
       botRegexSiege = /^\/siege/
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -117,6 +118,11 @@ function respond() {
   else if(request.text && botRegexapplicationlink.test(request.text)) {
     this.res.writeHead(200);
     postMessage("https://docs.google.com/forms/d/1EEZdnmnftR4OWcyE5z7IIsjBB2AvrzzkeFdPgzIGA5A/viewform");
+    this.res.end();
+  }
+  else if(request.text && botRegexhaters.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("http://i.imgur.com/piLQsKQ.gif");
     this.res.end();
   }
   else if(request.text && botRegexgetsomething.test(request.text)) {
