03c01e786f467ba1b5da058d496182695ea9a2d0
tedunni <tedunni@gmail.com>
2016-05-17 14:18:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -134,7 +134,9 @@ function respond() {
   }
   else if(request.text && botRegexgif.test(request.text)) {
     this.res.writeHead(200);
+    postMessage("1");
     var searchTerm = request.text.substr(5);
+    postMessage("2");
 
     /*this.res.writeHead(200);
     postMessage("http://gifsec.com/wp-content/uploads/GIF/2014/11/Rage-quit-Rage-Raging-Mad-Angry-You-lose-GIF.gif?gs=a");
@@ -142,11 +144,16 @@ function respond() {
     */
 
     request('http://api.giphy.com/v1/gifs/translate?s=' + searchTerm + '&api_key=dc6zaTOxFJmzC&rating=r', function (error, response, body) {
+    postMessage("2");
+    
     if (!error && response.statusCode == 200) {
+      postMessage("3");
       parsedData = JSON.parse(body),
       postMessage(parsedData.data.images.downsized.url);
+      postMessage("4");
       } 
-    });    
+    });
+    postMessage("5");
     this.res.end();
   }
   else if(request.text && botRegexgetsomething.test(request.text)) {
